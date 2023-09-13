56b15f752e657685a78eefccef1c1b7049f88ab4
Filip CIca <72666124+FC122@users.noreply.github.com>
2023-09-13 10:42:16 +0200

    Add advanced error handling

--- snipets.cy.js
+++ snipets.cy.js
@@ -239,3 +239,48 @@ module.exports = parseEmail
         }
       })
     })
+
+
+//advanced custom error handling
+/// <reference types="cypress" />
+let errors = []
+
+/**
+ * When test fails on any cypress command
+ * this ensures that all custom errors will be shown
+ * along with the cypress error of failed command.
+ */
+
+Cypress.on('fail', (error, runnable) => {
+  let errorMessage = ' '
+  if (errors.length > 0) {
+    for (let i = 0; i < errors.length; i++) {
+      errorMessage += `Url doesn't contain >>${errors[i]}<<\n`
+    }
+  }
+  errors = []
+  errorMessage += error.message
+  throw new Error(errorMessage)
+})
+
+describe('Check my activities page', () => {
+  let cookieList
+
+  afterEach('Handle errors', () => {
+    if (errors.length > 0) {
+      let errorMessage = ' '
+      for (let i = 0; i < errors.length; i++) {
+        errorMessage += `Url doesn't contain >>${errors[i]}<<\n`
+      }
+      errors = []
+      throw new Error(errorMessage)
+    }
+  })
+    
+it('Some test', ()=>{
+    if(){
+        errors.push('some error')
+    }
+})
+
+})
