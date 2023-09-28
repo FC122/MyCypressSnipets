3ef1a3fc54f569aa6144a210f29b72763bfc83c2
Filip CIca <72666124+FC122@users.noreply.github.com>
2023-09-28 13:41:25 +0200

    Update shouldNotBeShown fj

--- snipets.cy.js
+++ snipets.cy.js
@@ -321,6 +321,29 @@ const shouldNotBeShown = (subject, option) => () => {
     }
 }
 
+//upgraded version
+const shouldNotBeShown = (subject) => () => {
+    const isHiddenByParent = (element) => {
+        if (!element || element.length === 0) {
+            return false; // Element doesn't exist or reached the root
+        }
+        const display = element.css('display');
+        const visibility = element.css('visibility');
+        if (display === 'none' || visibility === 'hidden') {
+            return true; // Element is hidden by a parent
+        }
+        return isHiddenByParent(element.parent()); // Recursively check parent elements
+    };
+
+    if (isHiddenByParent(subject) || subject.length === 0) {
+        //Button is not shown
+        expect(true).to.be.true;
+    } else {
+        //Throw custom error
+        throw new Error('Element is shown when it should not be.');
+    }
+}
+
 Cypress.Commands.overwrite('should', (originalFn, subject, expectation, ...args) => {
     const customMatchers = {
         'have.backgroundColor': compareColor(args[0], 'backgroundColor'),
